0df6b9273894a69945ac9cc4913b429977b63c9c
Sergey Kozlov <dartess@mail.ru>
2022-10-29 15:21:30 +0400

    add routing based on wouter

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,32 +1,30 @@
-import { useState } from "react";
-
+import { useLocation } from "wouter";
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import QuizIcon from '@mui/icons-material/Quiz';
 
 import styles from './Layout.module.css'
 import { IconAyb } from "./view/IconAyb";
-import { PageValue } from "@/types/model";
 import { Router } from "@/components/Router/Router";
 import { Paper } from "@mui/material";
 
 export const Layout = () => {
-  const [page, setPage] = useState<PageValue>('alphabet');
+  const [location, setLocation] = useLocation();
 
   return <div className={styles.root}>
     <main className={styles.main}>
-      <Router page={page}/>
+      <Router />
     </main>
     <div className={styles.footer}>
       <Paper elevation={3}>
         <BottomNavigation
-          value={page}
+          value={location}
           onChange={(event, newValue) => {
-            setPage(newValue);
+            setLocation(newValue);
           }}
         >
-          <BottomNavigationAction label="Алфавит" icon={<IconAyb/>} value='alphabet'/>
-          <BottomNavigationAction label="Практика" icon={<QuizIcon/>} value='practice'/>
+          <BottomNavigationAction label="Алфавит" icon={<IconAyb/>} value='/alphabet'/>
+          <BottomNavigationAction label="Практика" icon={<QuizIcon/>} value='/practice'/>
         </BottomNavigation>
       </Paper>
     </div>
--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,19 +1,11 @@
-import type { PageValue } from "@/types/model";
+import { Redirect, Route, Switch } from "wouter";
 import { Alphabet } from "@/pages/alphabet/Alphabet/Alphabet";
 import { Practice } from "@/pages/practice/Practice";
-import { exhaustiveCheck } from "@/utils/exhaustiveCheck";
 
-type Props = {
-  page: PageValue;
-}
-
-export const Router = ({ page }: Props) => {
-  switch (page) {
-    case "alphabet":
-      return <Alphabet/>
-    case "practice":
-      return <Practice/>
-    default:
-      return exhaustiveCheck(page);
-  }
-}
+export const Router = () => (
+  <Switch>
+    <Route path="/alphabet" component={Alphabet}/>
+    <Route path="/practice" component={Practice}/>
+    <Redirect to='/alphabet'/>
+  </Switch>
+)
