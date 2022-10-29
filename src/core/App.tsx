92f6dc4b0ace179a65c0489ea362e851572f1e1b
Sergey Kozlov <dartess@mail.ru>
2022-10-29 22:14:06 +0400

    fix styles order

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 import { Layout } from "@/components/Layout/Layout";
 
 function App() {
   return (
+    <StyledEngineProvider injectFirst>
       <Layout />
+    </StyledEngineProvider>
   );
 }
 
