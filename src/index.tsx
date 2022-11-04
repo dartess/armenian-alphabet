b171ce738fb39eba9cd531e3aa07b66ed8c1627c
Sergey Kozlov <dartess@mail.ru>
2022-11-04 21:14:45 +0400

    test vh fix

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 import '@/core/index.css';
 import App from '@/core/App';
+import { fixIOsVh } from "@/core/vh";
+
+fixIOsVh()
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
