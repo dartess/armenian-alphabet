88b777d038b9109a001e726868aec6fef2843396
Sergey Kozlov <dartess@mail.ru>
2022-11-17 18:24:52 +0400

    add sounds in interface

--- src/components/Letter/Letter.tsx
+++ src/components/Letter/Letter.tsx
@@ -6,6 +6,7 @@ import { LetterUppercase } from '@/components/units/LetterUppercase';
 import { LetterLowercase } from '@/components/units/LetterLowercase';
 import { LetterTransliteration } from '@/components/units/LetterTransliteration';
 import { LetterIpa } from '@/components/units/LetterIpa';
+import { LetterAudio } from '@/components/LetterAudio/LetterAudio';
 
 import styles from './Letter.module.css';
 
@@ -52,6 +53,19 @@ export function Letter({ letter, state, showVariants }: Props) {
               sx={{ margin: '0 7px' }}
             />
             <LetterIpa letter={letter} showVariants={showVariants} />
+            {
+              showVariants
+              && (
+                <>
+                  <Divider
+                    orientation="vertical"
+                    flexItem
+                    sx={{ marginLeft: '7px' }}
+                  />
+                  <LetterAudio letter={letter} />
+                </>
+              )
+            }
           </Box>
         </div>
       </Box>
---
+++ src/components/LetterAudio/LetterAudio.tsx
@@ -0,0 +1,27 @@
+import { useAudio } from 'react-use';
+import IconButton from '@mui/material/IconButton';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+
+import type { LetterType } from '@/types/model';
+
+interface Props {
+  letter: LetterType;
+}
+
+export function LetterAudio({ letter: { audio } }: Props) {
+  const [audioElement, state, controls] = useAudio({ src: `/audio/letters/${audio}.ogg` });
+
+  return (
+    <>
+      <IconButton
+        aria-label="прослушать"
+        size="small"
+        onClick={() => controls.play()}
+        disabled={state.playing}
+      >
+        <PlayCircleOutlineIcon fontSize="inherit" />
+      </IconButton>
+      {audioElement}
+    </>
+  );
+}
