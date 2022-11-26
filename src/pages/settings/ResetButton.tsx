0bf037a014196b21501e03c42c8ddd9ee2101cfd
Sergey Kozlov <dartess@mail.ru>
2022-11-26 13:13:04 +0400

    remove warning class

--- src/pages/settings/ResetButton.tsx
+++ src/pages/settings/ResetButton.tsx
@@ -25,7 +25,6 @@ export const ResetButton = observer(() => {
 
   return (
     <Button
-      color="warning"
       variant="outlined"
       onClick={handleClick}
       endIcon={<DeleteForeverIcon />}
