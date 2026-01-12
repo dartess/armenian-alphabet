7b8747443819f1f4288e9b6070444633b4220ec7
Sergey Kozlov <dartess@mail.ru>
2026-01-12 13:43:47 +0400

    update @dartess/eslint-plugin

--- src/utils/useUpdatedRef.ts
+++ src/utils/useUpdatedRef.ts
@@ -3,6 +3,7 @@ import { useRef } from 'react';
 // https://css-tricks.com/dealing-with-stale-props-and-states-in-reacts-functional-components/
 export function useUpdatedRef<T>(data: T) {
   const ref = useRef(data);
+  // eslint-disable-next-line react-hooks/refs -- TODO remove this hooks after update react + useEffectEvent
   ref.current = data;
   return ref;
 }
